4060f3c0559ad69661c269c5a96a890bb7c8e60c
Staneesh <blkreasons@outlook.com>
2020-12-06 13:37:19 +0100

    ✨ Day 6 - Second part finished!

--- task6/main.py
+++ task6/main.py
@@ -2,12 +2,15 @@ file = open("data", "r")
 sum = 0
 for paragraph in file.read().split("\n\n"):
     buckets = [0] * 26
-    for character in paragraph:
-        letter_val = ord(character) - ord('a')
-        if letter_val >= 0 and letter_val <= 26:
-            buckets[letter_val] = 1
-    for letter in buckets:
-        sum = sum + letter
+    for line in paragraph.split("\n"):
+        for character in line:
+            letter_val = ord(character) - ord('a')
+            if letter_val >= 0 and letter_val <= 26:
+                buckets[letter_val] = buckets[letter_val] + 1
+
+    for place in buckets:
+        if place == len(paragraph.split("\n")):
+            sum = sum + 1      
         
 print(sum)
 file.close()
